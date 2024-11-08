f136df4573310a4f5c24ef8463fbeaea1b4fd6e4
pooja <poojasahu02071994>
2024-11-08 15:32:16 +0530

    control-flow

---
+++ if_else.py
@@ -0,0 +1,16 @@
+'''
+conditional statement (decision making)
+if-else
+type of conditional statement
+if-else| if-else lader|if-else nesting
+
+print (main script)--->simple statement(last most corner)
+if True:                ---->complex
+    print("if body")----4 space de kr likhna rahta h
+'''
+age=int(input("Enter age \n"))
+if age>=18:
+    print("eligible")
+else:
+    print("not")
+ 
---
+++ que.py
@@ -0,0 +1,15 @@
+#w ap
+charecter=(input("Enter char \n"))
+v="AEIOUaeiou"
+if charecter in v:
+    print("vowel")
+else:
+    print("not a vowel")
+
+#2nd approch
+# charecter=(input("Enter char \n"))
+# if charecter=='a'or charecter=='e'or charecter=='i'or charecter=='o'or charecter=='u':
+#     print("vowel")
+# else:
+#     print("not a vowel")
+
